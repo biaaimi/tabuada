bb3af906ea668730768dc3c37dd087ec1f3cd86d
biaaimi <biagioni.aimi@gmail.com>
2021-11-26 22:12:02 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" tabuada do numero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int m = 0;
+            int produto;
+
+            while (m <= 10)
+
+             {
+                produto = n * m;
+                Console.WriteLine($"{n} x {m} = {produto}");
+                m += 1;
+            }
+        }
+    }
+}
